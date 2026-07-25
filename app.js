2481d6ebcb8be98fa6ae64c932ce2c76148d07dd
harsh <harshsondagar@weetechsolution.com>
2026-07-25 11:11:44 +0530

    addeed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const circleBtn = document.querySelector(".circle-btn")
 const squareBtn = document.querySelector(".square-btn")
 const pencilBtn = document.querySelector(".pencil-btn")
 const ctx = canvas.getContext("2d")
+canvas.setAttribute('tabindex', '0');
+canvas.focus();
 
 let shapes = []
 let selectedSHape = null
@@ -19,210 +21,308 @@ let y = 0
 let resizing = false
 let pencilPath = []
 
-
-circleBtn.addEventListener("click", () => {
-    drawShapeType = "circle"
-    console.log(drawShapeType)
-
-})
-
-squareBtn.addEventListener("click", () => {
-    drawShapeType = "square"
-    console.log(drawShapeType)
-})
-
-
-pencilBtn.addEventListener("click", () => {
-    drawShapeType = "pen"
-    console.log(drawShapeType);
-})
-
-function isMouseInsideShape(mouseX, mouseY, shape) {
-    const minX = Math.min(shape.x, shape.x + shape.w)
-    const minY = Math.min(shape.y, shape.y + shape.h)
-    const maxX = Math.max(shape.x, shape.x + shape.w)
-    const maxY = Math.max(shape.y, shape.y + shape.h)
-
-    return mouseX >= minX &&
-        mouseX <= maxX &&
-        mouseY >= minY &&
-        mouseY <= maxY
-}
-
-canvas.addEventListener("mousedown", (e) => {
-    clicked = true
-    pencilPath = []
-    const rect = canvas.getBoundingClientRect();
-
-    x = e.clientX
-    y = e.clientY
-
-    if (selectedSHape && isHandleSelected(x, y, selectedSHape)) {
-        resizing = true  // ✅ set once on click
-        return
-    }
-
-    for (let i = 0; i < shapes.length; i++) {
-
-        if (isMouseInsideShape(x, y, shapes[i])) {
-
-            selectedSHape = shapes[i]
-            // resizeShape(selectedSHape)
-            break
-        }
-
-    }
-
-})
+let lastXforLine = 0
+let lastYforLine = 0
+let move = 10
 
 
 
-canvas.addEventListener("mousemove", (e) => {
-
-    if (!clicked) return
-    if (resizing) {
-        resizeShape(e.clientX, e.clientY, selectedSHape)
-
-        return
-    }
-
-    const rect = canvas.getBoundingClientRect();
-
-    const w = e.clientX - x
-    const h = e.clientY - y
-
-    if (drawShapeType === "pen") {
-        ctx.beginPath()
-        ctx.moveTo(x, y)
-        ctx.lineTo(e.clientX, e.clientY)
-        ctx.stroke()
-
-        x = e.clientX
-        y = e.clientY
-        pencilPath.push({ x, y })
-
-    } else {
-        // ✅ only clear for rect/circle preview
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawShape()
-
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 1;
-
-        if (drawShapeType === "circle") {
-            const radius = Math.sqrt(w * w + h * h) / 2
-            const centerX = x + w / 2
-            const centerY = y + h / 2
-            ctx.beginPath()
-            ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
-            ctx.stroke()
-        } else if (drawShapeType === "square") {
-            ctx.beginPath()
-            ctx.strokeRect(x, y, w, h)
-        }
-    }
-
-})
-
-canvas.addEventListener("mouseup", (e) => {
-    id++;
-    clicked = false
-    if (resizing) {
-        resizing = false  // ✅ just stop resizing, don't add new shape
-        drawShape()
-        return
-    }
-
-    resizing = false
-
-    const rect = canvas.getBoundingClientRect();
-
-    const w = e.clientX - x
-    const h = e.clientY - y
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawShape()
+let xp = 100
+let yp = 100
+myAvatar()
 
 
+// circleBtn.addEventListener("click", () => {
+//     drawShapeType = "circle"
+//     console.log(drawShapeType)
+
+// })
+
+// squareBtn.addEventListener("click", () => {
+//     drawShapeType = "square"
+//     console.log(drawShapeType)
+// })
+
+
+// pencilBtn.addEventListener("click", () => {
+//     drawShapeType = "pen"
+//     console.log(drawShapeType);
+// })
+
+// function isMouseInsideShape(mouseX, mouseY, shape) {
+//     const minX = Math.min(shape.x, shape.x + shape.w)
+//     const minY = Math.min(shape.y, shape.y + shape.h)
+//     const maxX = Math.max(shape.x, shape.x + shape.w)
+//     const maxY = Math.max(shape.y, shape.y + shape.h)
+
+//     return mouseX >= minX &&
+//         mouseX <= maxX &&
+//         mouseY >= minY &&
+//         mouseY <= maxY
+// }
+
+// canvas.addEventListener("mousedown", (e) => {
+
+
+//     clicked = true
+//     pencilPath = []
+//     const rect = canvas.getBoundingClientRect();
+
+//     x = e.clientX
+//     y = e.clientY
+
+
+//     console.log(x, y);
+
+
+//     if (selectedSHape && isHandleSelected(x, y, selectedSHape)) {
+//         resizing = true  // ✅ set once on click
+//         return
+//     }
+
+//     for (let i = 0; i < shapes.length; i++) {
+
+//         if (isMouseInsideShape(x, y, shapes[i])) {
+
+//             selectedSHape = shapes[i]
+//             // resizeShape(selectedSHape)
+//             break
+//         }
+
+//     }
+
+// })
+
+
+
+// canvas.addEventListener("mousemove", (e) => {
+
+//     if (!clicked) return
+//     if (resizing) {
+//         resizeShape(e.clientX, e.clientY, selectedSHape)
+
+//         return
+//     }
+
+
+
+//     const rect = canvas.getBoundingClientRect();
+
+//     const w = e.clientX - x
+//     const h = e.clientY - y
+
+//     if (drawShapeType === "pen") {
+//         ctx.beginPath()
+//         ctx.moveTo(x, y)
+//         ctx.lineTo(e.clientX, e.clientY)
+//         ctx.stroke()
+
+//         x = e.clientX
+//         y = e.clientY
+//         pencilPath.push({ x, y })
+
+//     } else {
+//         // ✅ only clear for rect/circle preview
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//         drawShape()
+
+//         ctx.strokeStyle = 'black';
+//         ctx.lineWidth = 1;
+
+//         if (drawShapeType === "circle") {
+//             const radius = Math.sqrt(w * w + h * h) / 2
+//             const centerX = x + w / 2
+//             const centerY = y + h / 2
+//             ctx.beginPath()
+//             ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
+//             ctx.stroke()
+//         } else if (drawShapeType === "square") {
+//             ctx.beginPath()
+//             ctx.strokeRect(x, y, w, h)
+//         }
+//     }
+
+// })
+
+// canvas.addEventListener("mouseup", (e) => {
+//     id++;
+//     clicked = false
+//     if (resizing) {
+//         resizing = false  // ✅ just stop resizing, don't add new shape
+//         drawShape()
+//         return
+//     }
+
+
+
+//     resizing = false
+
+//     const rect = canvas.getBoundingClientRect();
+
+//     const w = e.clientX - x
+//     const h = e.clientY - y
+
+//     if (!lastXforLine && !lastXforLine) {
+//         lastXforLine = e.clientX
+//         lastYforLine = e.clientY
+
+//     }
+
+
+
+
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     drawShape()
+
+
+//     ctx.strokeStyle = 'black';
+//     ctx.lineWidth = 1
+//     ctx.beginPath()
+
+//     if (drawShapeType === "square") {
+//         ctx.strokeRect(x, y, w, h)
+//         shapes = [...shapes, { type: "square", id: id, x, y, w, h }]
+//     } else if (drawShapeType === "circle") {
+//         const radius = Math.sqrt(w * w + h * h) / 2
+//         const centerX = x + w / 2
+//         const centerY = y + h / 2
+//         ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
+//         ctx.stroke()
+//         shapes = [...shapes, { type: "circle", id: id, x: centerX, y: centerY, radius: radius }]
+//     } else if (drawShapeType === "pen") {
+//         shapes = [...shapes, { type: "pen", id: id, points: pencilPath }]
+//         drawShape()
+//     }
+
+//     console.log(lastXforLine, lastYforLine);
+//     console.log(x, y);
+
+
+//     ctx.beginPath()
+//     ctx.moveTo(lastXforLine, lastYforLine)
+//     ctx.lineTo(x, y)
+//     ctx.stroke();
+
+//     console.log("drawing line");
+
+// })
+
+
+// function drawShape() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     shapes.forEach(shape => {
+//         ctx.strokeStyle = 'black';
+//         ctx.lineWidth = 1
+//         ctx.beginPath()
+//         // console.log(shape)
+//         if (selectedSHape && selectedSHape.id === shape.id) {
+
+//             ctx.strokeStyle = "blue";
+//             ctx.lineWidth = 3;
+
+
+//             ctx.strokeRect(shape.x, shape.y, shape.w, shape.h);
+//             ctx.fillStyle = "blue";
+//             ctx.fillRect(
+//                 shape.x + shape.w - HANDLE_SIZE / 2,
+//                 shape.y + shape.h - HANDLE_SIZE / 2,
+//                 HANDLE_SIZE,
+//                 HANDLE_SIZE
+//             );
+
+
+//         }
+
+//         if (shape.type === "square") {
+//             ctx.strokeRect(shape.x, shape.y, shape.w, shape.h)
+//         } else if (shape.type === "circle") {
+//             console.log(shape.type)
+//             ctx.arc(shape.x, shape.y, shape.radius, 0, 2 * Math.PI)
+//             ctx.stroke()
+//         } else if (shape.type === "pen") {
+//             shape.points.forEach((p, i) => {
+//                 console.log(p.x)
+//                 if (i === 0) {
+//                     ctx.moveTo(p.x, p.y)
+//                 } else {
+//                     ctx.lineTo(p.x, p.y)  // ✅ draw to current pos
+//                 }
+//             })
+//             ctx.stroke()
+//         }
+//     })
+// }
+
+// function resizeShape(x, y, shape) {
+//     shape.w = x - shape.x  // no handle coords needed!
+//     shape.h = y - shape.y
+//     drawShape()
+// }
+// function isHandleSelected(x, y, shape) {
+
+//     const handleX = shape.x + shape.w - HANDLE_SIZE / 2
+//     const handleY = shape.y + shape.h - HANDLE_SIZE / 2
+
+//     return x >= handleX &&
+//         x <= handleX + HANDLE_SIZE &&
+//         y >= handleY &&
+//         y <= handleY + HANDLE_SIZE
+// }
+
+
+
+function myAvatar(pos) {
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 1
-    ctx.beginPath()
+    ctx.fillStyle = '#ff5733'
 
-    if (drawShapeType === "square") {
-        ctx.strokeRect(x, y, w, h)
-        shapes = [...shapes, { type: "square", id: id, x, y, w, h }]
-    } else if (drawShapeType === "circle") {
-        const radius = Math.sqrt(w * w + h * h) / 2
-        const centerX = x + w / 2
-        const centerY = y + h / 2
-        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
-        ctx.stroke()
-        shapes = [...shapes, { type: "circle", id: id, x: centerX, y: centerY, radius: radius }]
-    } else if (drawShapeType === "pen") {
-        shapes = [...shapes, { type: "pen", id: id, points: pencilPath }]
-        drawShape()
+
+    if (pos === 'up') {
+
+        yp -= 1
+        // xp -= 10
+    } else if (pos === 'down') {
+        console.log(pos);
+
+        yp += 1
+        // xp += 10
     }
+
+    console.log(xp, yp);
+
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath()
+    ctx.arc(xp, yp, 20, 0, 2 * Math.PI)
+
+    ctx.fill();
+    ctx.stroke()
+}
+
+
+
+
+
+
+
+canvas.addEventListener('keydown', (e) => {
+
+    if (e.key === 'ArrowUp') {
+        myAvatar('up')
+    }
+
+    if (e.key === 'ArrowDown') {
+        myAvatar('down')
+    }
+
+    // if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) {
+    //     e.preventDefault();
+
+
+    //     move += 10
+    //     myAvatar(move, move)
+
+    // }
 })
 
-
-function drawShape() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    shapes.forEach(shape => {
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 1
-        ctx.beginPath()
-        // console.log(shape)
-        if (selectedSHape && selectedSHape.id === shape.id) {
-
-            ctx.strokeStyle = "blue";
-            ctx.lineWidth = 3;
-
-
-            ctx.strokeRect(shape.x, shape.y, shape.w, shape.h);
-            ctx.fillStyle = "blue";
-            ctx.fillRect(
-                shape.x + shape.w - HANDLE_SIZE / 2,
-                shape.y + shape.h - HANDLE_SIZE / 2,
-                HANDLE_SIZE,
-                HANDLE_SIZE
-            );
-
-
-        }
-
-        if (shape.type === "square") {
-            ctx.strokeRect(shape.x, shape.y, shape.w, shape.h)
-        } else if (shape.type === "circle") {
-            console.log(shape.type)
-            ctx.arc(shape.x, shape.y, shape.radius, 0, 2 * Math.PI)
-            ctx.stroke()
-        } else if (shape.type === "pen") {
-            shape.points.forEach((p, i) => {
-                console.log(p.x)
-                if (i === 0) {
-                    ctx.moveTo(p.x, p.y)
-                } else {
-                    ctx.lineTo(p.x, p.y)  // ✅ draw to current pos
-                }
-            })
-            ctx.stroke()
-        }
-    })
-}
-
-function resizeShape(x, y, shape) {
-    shape.w = x - shape.x  // no handle coords needed!
-    shape.h = y - shape.y
-    drawShape()
-}
-function isHandleSelected(x, y, shape) {
-
-    const handleX = shape.x + shape.w - HANDLE_SIZE / 2
-    const handleY = shape.y + shape.h - HANDLE_SIZE / 2
-
-    return x >= handleX &&
-        x <= handleX + HANDLE_SIZE &&
-        y >= handleY &&
-        y <= handleY + HANDLE_SIZE
-}
